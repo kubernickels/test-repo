f0ec98bfda8bb686bf8205af759c4cec005c8ea9
Jared Edwards <jared@kubefirst.com>
2023-02-17 13:36:34 -0700

    whoops

--- terraform/maint.tf
+++ terraform/maint.tf
@@ -1,16 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket   = "<KUBEFIRST_STATE_STORE_BUCKET>"
-    key      = "terraform/civo/tfstate.tf"
-    endpoint = "https://objectstore.<CLOUD_REGION>.civo.com"
-
-    region = "<CLOUD_REGION>"
-
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    force_path_style            = true
-  }
   required_providers {
     civo = {
       source = "civo/civo"
